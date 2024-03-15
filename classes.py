d1d6998c357633a7c3db26fd4f3c8eec66d88c91
Екатерина Кудряшова <katyushka555@yandex.ru>
2024-03-15 13:56:24 +0400

    Написание функций для создания БД и добавления данных в БД

--- classes.py
+++ classes.py
@@ -1,9 +1,12 @@
+from config import config
+import psycopg2
 import requests
 
 from config import EMPLOYERS_URL, VACANCIES_URL
 
 
 class Employer:
+    """Класс работодателя"""
 
     def __init__(self, employer_id: str, employer_name: str, employer_vacancies: int, employer_url: str):
         self.employer_id = employer_id
@@ -13,19 +16,25 @@ class Employer:
 
     @classmethod
     def initiate_from_hh(cls, em_ids: list):
+        """Создает список с экземплярами класса по заданным параметрам"""
+
         employers_list = []
         for em_id in em_ids:
-            response = requests.get(url=f"{EMPLOYERS_URL}{em_id}").json()
-            employer_id = response['id']
-            employer_name = response['name']
-            employer_vacancies = response['open_vacancies']
-            employer_url = response['alternate_url']
-            employer = Employer(employer_id, employer_name, employer_vacancies, employer_url)
-            employers_list.append(employer)
+            try:
+                response = requests.get(url=f"{EMPLOYERS_URL}{em_id}").json()
+                employer_id = response['id']
+                employer_name = response['name']
+                employer_vacancies = response['open_vacancies']
+                employer_url = response['alternate_url']
+                employer = Employer(employer_id, employer_name, employer_vacancies, employer_url)
+                employers_list.append(employer)
+            except KeyError:
+                raise KeyError('id not found')
         return employers_list
 
 
 class Vacancy:
+    """Класс вакансии"""
 
     def __init__(self,
                  employer_id: str,
@@ -45,6 +54,8 @@ class Vacancy:
 
     @classmethod
     def initiate_from_hh(cls, employer_id: str):
+        """Создает список с экземплярами класса по заданным параметрам"""
+
         params = {
             'page': 0,
             'per_page': 100,
@@ -67,12 +78,121 @@ class Vacancy:
 
     @staticmethod
     def validate_salary_from(salary):
+        """Возвращает 0 если не указана минимальная зарплата"""
         if not salary:
             return 0
         return salary
 
     @staticmethod
     def validate_salary_to(salary_from, salary_to):
+        """Возвращает максимальную зарплату равную минимальной, если максимальная не указана"""
+
         if not salary_to:
             salary_to = salary_from
         return salary_to
+
+
+class DBManager:
+
+    @classmethod
+    def get_companies_and_vacancies_count(cls, database_name):
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+
+        params = config()
+        conn = psycopg2.connect(dbname=database_name, **params)
+        with conn.cursor() as cur:
+            cur.execute("""
+            SELECT employers.employer_name as employer, COUNT(*)
+            FROM employers
+            JOIN vacancies using (employer_id)
+            GROUP BY employer
+            """)
+            response = cur.fetchall()
+        for r in response:
+            print(f"{r[0]} - {r[1]} вакансий")
+        conn.close()
+
+    @classmethod
+    def get_all_vacancies(cls, database_name):
+        """Получает список всех вакансий с указанием названий компании и
+        вакансии, зарплаты и ссылки на вакансию"""
+
+        params = config()
+        conn = psycopg2.connect(dbname=database_name, **params)
+        with conn.cursor() as cur:
+            cur.execute("""
+            SELECT
+            employers.employer_name,
+            vacancies.vacancy_name,
+            vacancies.salary_from,
+            vacancies.salary_to,
+            vacancies.vacancy_url
+            FROM employers
+            JOIN vacancies USING (employer_id)
+            """)
+            response = cur.fetchall()
+        for r in response:
+            print(f"{r[0]} / {r[1]} / {r[2]}-{r[3]} RUR / ссылка {r[4]}")
+
+    @classmethod
+    def get_avg_salary(cls, database_name):
+        """Получает среднюю зарплату по вакансиям"""
+
+        params = config()
+        conn = psycopg2.connect(dbname=database_name, **params)
+        with conn.cursor() as cur:
+            cur.execute("""
+            SELECT AVG((salary_from + salary_to)/2)
+            FROM vacancies
+            """)
+            response = cur.fetchall()
+        print(f"Средняя зарплата по всем вакансиям базы данных - {round(response[0][0])} рублей")
+
+    @classmethod
+    def get_vacancies_with_higher_salary(cls, database_name):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
+
+        params = config()
+        conn = psycopg2.connect(dbname=database_name, **params)
+        with conn.cursor() as cur:
+            cur.execute("""SELECT 
+            employers.employer_name,
+            vacancies.vacancy_name,
+            vacancies.salary_from,
+            vacancies.salary_to,
+            vacancies.vacancy_url
+            FROM employers
+            JOIN vacancies USING (employer_id)
+            WHERE ((vacancies.salary_from + vacancies.salary_to)/2) >
+            (SELECT AVG((salary_from + salary_to)/2) FROM vacancies)""")
+            response = cur.fetchall()
+        cls.get_avg_salary(database_name)
+        print("\nЗарплата по данным вакансиям выше средней по всей базе:\n")
+        for r in response:
+            print(f"{r[0]} / {r[1]} / {r[2]}-{r[3]} RUR / ссылка {r[4]}")
+
+    @classmethod
+    def get_vacancies_with_keyword(cls, database_name, query):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
+
+        params = config()
+        conn = psycopg2.connect(dbname=database_name, **params)
+        with conn.cursor() as cur:
+            cur.execute(f"""
+            SELECT
+            employers.employer_name,
+            vacancies.vacancy_name,
+            vacancies.salary_from,
+            vacancies.salary_to,
+            vacancies.vacancy_url
+            FROM employers
+            JOIN vacancies USING (employer_id)
+            WHERE vacancies.vacancy_name LIKE '%{query.lower()}%'
+            """)
+            response = cur.fetchall()
+        if not response:
+            print("\nПо вашему запросу ничего не найдено\n")
+        else:
+            print("\nПо вашему запросу найдены следующие вакансии:\n")
+            for r in response:
+                print(f"{r[0]} / {r[1]} / {r[2]}-{r[3]} RUR / ссылка {r[4]}")
--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from config import EMPLOYERS_URL
-from classes import Employer, Vacancy
+from config import config
+from utils import create_database, save_data_to_database, user_interactive
 
 if __name__ == '__main__':
-    hh_key = EMPLOYERS_URL
-    employers_ids = ['80',  # Альфа-Банк
-                     '1740',  # Яндекс
-                     '4181',  # ВТБ
-                     '4219',  # Tele2
-                     '1373',  # "Аэрофлот"
+    employers_ids = ['80',     # Альфа-Банк
+                     '1740',   # Яндекс
+                     '4181',   # ВТБ
+                     '4219',   # Tele2
+                     '1373',   # "Аэрофлот"
                      '39305',  # Газпром нефть
-                     '3388',  # Газпромбанк
+                     '3388',   # Газпромбанк
                      '15478',  # VK
-                     '4233',  # X5 Group
-                     '78638'  # Тинькофф
+                     '4233',   # X5 Group
+                     '78638'   # Тинькофф
                      ]
 
-    employers_list = Employer.initiate_from_hh(employers_ids)
-    print(employers_list[9].employer_name)
+    params = config()
 
-    vacancies_list = Vacancy.initiate_from_hh(employers_list[9].employer_id)
-    for x in vacancies_list:
-        print(x.currency)
+    create_database("сoursework5", params)
+    save_data_to_database(employers_ids, "сoursework5", params)
+    user_interactive("сoursework5")
